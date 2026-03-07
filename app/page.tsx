5db44a4353890cbb8858651aac8c538b4307175d
Daniil Surin <wkingsurin@gmail.com>
2026-03-07 15:59:36 +0500

    fix: add missing box-border to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<div className="w-full">
-			<div className="container mx-auto flex flex-col gap-4 max-w-3xl text-center pt-40">
+			<div className="container box-border px-4 mx-auto flex flex-col gap-4 max-w-3xl text-center mt-40">
 				<h1 className="font-poppins text-5xl/18 text-dark font-bold">
 					Manage your tasks and organize processes
 				</h1>
