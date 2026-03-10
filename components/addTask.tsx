c683e9d7cb0cee9d2e1c98615573cccaea625869
Daniil Surin <wkingsurin@gmail.com>
2026-03-10 14:56:50 +0500

    feat(createTask): add onClick handler to switch AddTask with CreateTask

--- components/addTask.tsx
+++ components/addTask.tsx
@@ -2,9 +2,13 @@
 
 import { Plus } from "lucide-react";
 
-export default function AddTask() {
+interface IAddTaskProps {
+	onClick: () => void
+}
+
+export default function AddTask({ onClick }: IAddTaskProps) {
 	return (
-		<li className="flex gap-1 items-center text-dark-500 bg-dark-50 hover:bg-dark-100 hover:text-dark-700 rounded-md py-[10px] px-3">
+		<li className="flex gap-1 items-center max-h-10 text-dark-500 bg-dark-50 hover:bg-dark-100 hover:text-dark-700 rounded-md py-[10px] px-3" onClick={onClick}>
 			<Plus size={16} />
 			<span>Add new task</span>
 		</li>
