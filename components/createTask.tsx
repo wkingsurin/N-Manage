380d3f26426fe1036acb6f2eaf98dbb25f8b19a5
Daniil Surin <wkingsurin@gmail.com>
2026-03-10 14:51:59 +0500

    feat(createTask): add handlers, replace base textarea with Textarea ui component

--- components/createTask.tsx
+++ components/createTask.tsx
@@ -2,19 +2,29 @@
 
 import { X } from "lucide-react";
 import { Button } from "./ui/button";
+import { Textarea } from "./ui/textarea";
 
-export default function CreateTask() {
+interface ICreateTaskProps {
+	onChange: (text: string) => void;
+	onAddNewTask: () => void;
+  onCloseNewTask: () => void;
+}
+
+export default function CreateTask({ onChange, onAddNewTask, onCloseNewTask }: ICreateTaskProps) {
 	return (
 		<li className="flex flex-col gap-[10px] items-end justify-between">
-			<textarea
+			<Textarea
 				name="textarea"
 				id="textarea"
 				placeholder="Typing your task..."
 				className="w-full bg-white hover:shadow-md rounded-md py-[10px] px-3 outline-none resize-none field-sizing-content min-h-[57px] max-h-[92]"
-			></textarea>
+				onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+					onChange(e.target.value)
+				}
+			/>
 			<div className="flex gap-2">
-				<Button className="border-md py-[10px] px-4">Create</Button>
-				<Button className="bg-dark-300 hover:bg-dark-500 border-md">
+				<Button className="border-md py-[10px] px-4" onClick={onAddNewTask}>Create</Button>
+				<Button className="bg-dark-300 hover:bg-dark-500 border-md" onClick={onCloseNewTask}>
 					<X size={16} />
 				</Button>
 			</div>
