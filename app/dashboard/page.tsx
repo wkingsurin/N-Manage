735c38c01ba76f729a2c9812baefcf1235462b36
Daniil Surin <wkingsurin@gmail.com>
2026-03-12 20:54:02 +0500

    refactor: remove unused code

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default async function Dashboard() {
 	}
 
 	return (
-		<div className="w-full" style={{height: "calc(100vh - 60px - 10px)"}}>
+		<div className="w-full" style={{ height: "calc(100vh - 60px - 10px)" }}>
 			<div className="container mx-auto h-[100%] pb-[10px] box-border px-4">
 				<div className="flex flex-col items-start text-dark bg-white broder border-dark-50 shadow-md rounded-md h-[100%]">
 					<h1 className="font-poppins font-semibold text-xl py-4 px-5">
@@ -22,43 +22,6 @@ export default async function Dashboard() {
 						<Card title="Today" period="today" />
 						<Card title="This week" period="week" />
 						<Card title="This month" period="month" />
-						{/* <div className="flex flex-col p-3 bg-pale-blue rounded-md border border-dark-50 gap-3 w-[33.333%]">
-							<span className="font-poppins font-semibold text-base">
-								Today
-							</span>
-							<ul className="flex flex-col gap-2">
-								<li className="flex gap-[10px] items-center justify-between bg-white hover:shadow-md rounded-md py-[10px] px-3">
-									<div className="flex gap-1 items-center">
-										<input
-											type="text"
-											value="Create Header Component"
-											contentEditable="false"
-										/>
-										<span>
-											<Pencil size={16} />
-										</span>
-									</div>
-									<span>
-										<Check size={16} className="hover:stroke-completed" />
-									</span>
-								</li>
-								<li className="flex gap-1 items-center text-dark-500 bg-dark-50 hover:bg-dark-100 hover:text-dark-700 rounded-md py-[10px] px-3">
-									<Plus size={16} />
-									<span>Add new task</span>
-								</li>
-							</ul>
-						</div>
-						<div className="flex flex-col p-3 bg-pale-blue rounded-md border border-dark-50 gap-3 w-[33.333%]">
-							<span className="font-poppins font-semibold text-base">
-								Today
-							</span>
-							<ul className="flex flex-col gap-2">
-								<li className="flex gap-1 items-center text-dark-500 bg-dark-50 hover:bg-dark-100 hover:text-dark-700 rounded-md py-[10px] px-3">
-									<Plus size={16} />
-									<span>Add new task</span>
-								</li>
-							</ul>
-						</div> */}
 					</div>
 				</div>
 			</div>
