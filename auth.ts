4f9fc8b1d369981d8f5b385094d4f67ddade3069
Daniil Surin <wkingsurin@gmail.com>
2026-03-15 14:24:43 +0500

    feat(auth): extend auth session
    - Added callbacks for saving user.id in cookie

--- auth.ts
+++ auth.ts
@@ -51,4 +51,18 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 	session: {
 		strategy: "jwt",
 	},
+	callbacks: {
+		async jwt({ token, user }) {
+			if (user) {
+				token.id = user.id;
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			if (session.user) {
+				session.user.id = token.id as string;
+			}
+			return session;
+		},
+	},
 });
