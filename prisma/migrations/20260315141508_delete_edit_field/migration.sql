2d18ad99f1c99cf30bdf8a481ea1e3a0b2875401
Daniil Surin <wkingsurin@gmail.com>
2026-03-15 19:28:50 +0500

    refactor(prisma.schema): delete an unnecessary 'edit' column

---
+++ prisma/migrations/20260315141508_delete_edit_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `edit` on the `tasks` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tasks" DROP COLUMN "edit";
