6a288555d009ab4b48c1915a0d9a02a7a13df7c6
Daniil Surin <wkingsurin@gmail.com>
2026-03-23 12:00:46 +0500

    refactor: remove unnecessary contexts, providers and custom hooks

--- components/contexts/creatingContext.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { NewTaskContextType } from "@/app/types/new-task.types";
-
-export const NewTaskContext = createContext<NewTaskContextType | null>(null);
