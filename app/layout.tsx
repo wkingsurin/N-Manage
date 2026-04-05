2f059c7ce1a2df56f3c3b79827a409e9040e2a10
Daniil Surin <wkingsurin@gmail.com>
2026-04-05 22:45:33 +0500

    feat(root layout): render components and setup theme
    - Rendered Overlay
    - Setup ThemeHydrator
    - Setup colorSchema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
 import { Poppins, Inter } from "next/font/google";
 import "./globals.css";
+import { cookies } from "next/headers";
 
 import AuthProvider from "@/components/providers/session-provider";
 
 import Header from "@/components/header";
+import Overlay from "@/components/shared/Overlay";
+import ThemeHydrator from "@/lib/theme-hydrator";
+import { Theme } from "@/lib/ui.store";
 
 const poppins = Poppins({
 	variable: "--font-poppins",
@@ -27,10 +31,15 @@ export default async function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const cookiesStore = await cookies();
+	const theme = (cookiesStore.get("theme")?.value as Theme) || "light";
+
 	return (
-		<html lang="en">
-			<body className={`${poppins.variable} ${inter.variable} antialiased`}>
+		<html lang="en" className={theme} style={{ colorScheme: theme }}>
+			<body className={`${poppins.variable} ${inter.variable} antialiased dark:bg-gloomy`}>
 				<AuthProvider>
+					<ThemeHydrator theme={theme} />
+					<Overlay />
 					<Header />
 					{children}
 				</AuthProvider>
